008c1debaba136502e2cb47ce31a94399bea1d96
Nao Takegawa <nao.takegawa316@gmail.com>
2017-05-23 08:07:41 +0900

    create table

--- table_example.php
+++ table_example.php
@@ -2,12 +2,14 @@
 <html>
   <head>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
     <style type="text/css">
     th, td {
       border-bottom: 1px solid #ddd;
      }
      .column1 {
        background-color: red;
+       transition: height 2s;
      }
      .column1:hover {
        height: 100px;
@@ -17,6 +19,16 @@
      }
      .column3 {
        background-color: green;
+     }
+     div.test {
+       width: 100px;
+       height: 100px;
+       background: red;
+       -webkit-transition: width 2s; /* For Safari 3.1 to 6.0 */
+       transition: width 2s;
+     }
+     div:hover {
+       width: 300px;
      }
     </style>
   </head>
@@ -28,7 +40,7 @@
         <th colspan="2">C23</th>
       </tr>
       <tr id="row1">
-        <td class="column1" style="height: 50px; transition: height 2s;"><span class="content">1-1</span></td>
+        <td class="column1" ><span class="content">1-1</span></td>
         <td class="column2" rowspan="3"><span class="column2 content">1-2</span></td>
         <td class="column3" rowspan="1"><span class="column3a content">1-3</span></td>
       </tr>
@@ -47,6 +59,8 @@
         <td class="column3">4-3</td>
       </tr>
     </table>
+    <br>
+    <div class="test"></div>
     <footer>
     </footer>
   </body>
