f2ae1a6de69ea711624373c2fbcf94fcc5643592
Nao Takegawa <nao.takegawa316@gmail.com>
2017-05-25 00:22:13 +0900

    create logic for table position

---
+++ table_position_array.php
@@ -0,0 +1,45 @@
+<?php
+$array    = [];
+$array[1] = 4;
+$array[2] = 2;
+$array[3] = 4;
+$array[4] = 1;
+
+$col_num = 1;
+
+$date_range = 5;
+
+var_dump($array);
+
+for ($i = 1; $i <= sizeof($array); $i++) {
+  if ($array[$i] == 0) {
+    $array[$i] = $date_range;
+    break;
+  }
+  $col_num++;
+}
+if (sizeof($array) < $col_num) {
+  $array[$col_num] = $date_range;
+}
+
+var_dump($array);
+
+echo "<br>";
+echo "colnum: $col_num";
+echo "<br>";
+foreach ($array as $key => $value) {
+  if ($array[$key] > 0) {
+    $array[$key]--;
+  }
+}
+
+var_dump($array);
+echo "<br>";
+foreach ($array as $key => $value) {
+  if ($array[$key] > 0) {
+    $array[$key]--;
+  }
+}
+
+var_dump($array);
+?>
