d05996eeebd18e1ddacac3ac5aa6776734d5bae6
Nao Takegawa <nao.takegawa316@gmail.com>
2017-06-18 14:53:08 +0900

    add scroll:overflow

--- table_example.php
+++ table_example.php
@@ -7,7 +7,6 @@
     table {
       /*table-layout: fixed;*/
       position: absolute;
-      left: 400px;
       border: 0;
     }
     th, td {
@@ -64,6 +63,34 @@
       position: absolute;
       top: 132px;
     }
+    #row6 {
+      position: absolute;
+      top: 165px;
+    }
+    #row7 {
+      position: absolute;
+      top: 198px;
+    }
+    #row8 {
+      position: absolute;
+      top: 231px;
+    }
+    #row9 {
+      position: absolute;
+      top: 264px;
+    }
+    #row10 {
+      position: absolute;
+      top: 297px;
+    }
+    #row11 {
+      position: absolute;
+      top: 330px;
+    }
+    #row12 {
+      position: absolute;
+      top: 363px;
+    }
     @keyframes move1 {
       from {height: 0px;}
       to {height: 31px;}
@@ -79,75 +106,127 @@
     </style>
   </head>
   <body>
-  <?php /*require_once("lib/idiorm.php") */?>
-  <?php
-    /*
-    ORM::configure('mysql:host=localhost;dbname=test_database');
-    ORM::configure('username', 'root');
-    ORM::configure('password', 'jiji4649');
-
-    $base_width = 25;
-    $rooms = ORM::for_table('rooms')->find_many();
-    foreach ($rooms as $room) {
-      echo $room->name;
-      echo "<br>";
-      $event_counts = ORM::for_table('events')
-                      ->where('room_id', $room->id)
-                      ->count();
-      echo $event_counts;
-      echo "<br>";
-      $width = ($base_width * $event_counts) + (2 * $event_counts);
-      echo "width: $width";
-      echo "<br>";
-    }
-    */
-  ?>
-    <header>
-    </header>
     <center>
-      <table class="target_table">
-        <tr id="row1">
-          <td class="header1">RoomA</th>
-          <td class="header2">RoomB</th>
-        </tr>
-        <tr id="row2">
-          <td class="column1"><div class="class1 class2 class3" style="background-color: blue; animation: move2 1s; animation-fill-mode: forwards;">1-1</div></td>
-          <td class="column2"><div class="class1 class2 class3" style="background-color: pink; animation: move3 1s; animation-fill-mode: forwards;">1-2</div></td>
-          <td class="column3"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-3</div></td>
-          <td class="column4"><div class="class1 class2 class3">1-4</div></td>
-          <td class="column5"><div class="class1 class2 class3" style="background-color: blue; animation: move2 1s; animation-fill-mode: forwards;">1-5</div></td>
-          <td class="column6"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-6</div></td>
-        </tr>
-        <tr id="row3">
-          <td class="column1"><div class="class1 class2 class3">2-1</div></td>
-          <td class="column2"><div class="class1 class2 class3">2-2</div></td>
-          <td class="column3"><div class="class1 class2 class3">2-3</div></td>
-          <td class="column4"><div class="class1 class2 class3" style="background-color: pink; animation: move3 1s; animation-fill-mode: forwards;">2-4</div></td>
-          <td class="column5"><div class="class1 class2 class3">2-5</div></td>
-          <td class="column6"><div class="class1 class2 class3">2-6</div></td>
-        </tr>
-        <tr id="row4">
-          <td class="column1"><div class="class1 class2 class3">3-1</div></td>
-          <td class="column2"><div class="class1 class2 class3">3-2</div></td>
-          <td class="column3"><div class="class1 class2 class3" style="background-color: blue; animation: move2 1s; animation-fill-mode: forwards;">3-3</div></td>
-          <td class="column4"><div class="class1 class2 class3">3-4</div></td>
-          <td class="column5"><div class="class1 class2 class3">3-5</div></td>
-          <td class="column6"><div class="class1 class2 class3">3-6</div></td>
-        </tr>
-        <tr id="row5">
-          <td class="column1"><div class="class1 class2 class3">4-1</div></td>
-          <td class="column2"><div class="class1 class2 class3">4-2</div></td>
-          <td class="column3"><div class="class1 class2 class3">4-3</div></td>
-          <td class="column4"><div class="class1 class2 class3">4-4</div></td>
-          <td class="column5"><div class="class1 class2 class3">4-5</div></td>
-          <td class="column6"><div class="class1 class2 class3">4-6</div></td>
-          <td class="column7"><div class="class1 class2 class3">4-7</div></td>
-          <td class="column8"><div class="class1 class2 class3">4-8</div></td>
-        </tr>
-      </table>
+      <div class="outside" style="position: absolute; left: 400px; width: 100px; height: 150px; overflow: scroll;">
+        <table class="target_table">
+          <tr id="row1">
+            <td class="header1">RoomA</th>
+            <td class="header2">RoomB</th>
+          </tr>
+          <tr id="row2">
+            <td class="column1"><div class="class1 class2 class3" style="background-color: blue; animation: move2 1s; animation-fill-mode: forwards;">1-1</div></td>
+            <td class="column2"><div class="class1 class2 class3" style="background-color: pink; animation: move3 1s; animation-fill-mode: forwards;">1-2</div></td>
+            <td class="column3"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-3</div></td>
+            <td class="column4"><div class="class1 class2 class3">1-4</div></td>
+            <td class="column5"><div class="class1 class2 class3" style="background-color: blue; animation: move2 1s; animation-fill-mode: forwards;">1-5</div></td>
+            <td class="column6"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-6</div></td>
+            <td class="column7"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-7</div></td>
+            <td class="column8"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-8</div></td>
+          </tr>
+          <tr id="row3">
+            <td class="column1"><div class="class1 class2 class3">2-1</div></td>
+            <td class="column2"><div class="class1 class2 class3">2-2</div></td>
+            <td class="column3"><div class="class1 class2 class3">2-3</div></td>
+            <td class="column4"><div class="class1 class2 class3" style="background-color: pink; animation: move3 1s; animation-fill-mode: forwards;">2-4</div></td>
+            <td class="column5"><div class="class1 class2 class3">2-5</div></td>
+            <td class="column6"><div class="class1 class2 class3">2-6</div></td>
+            <td class="column7"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-7</div></td>
+            <td class="column8"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-8</div></td>
+          </tr>
+          <tr id="row4">
+            <td class="column1"><div class="class1 class2 class3">3-1</div></td>
+            <td class="column2"><div class="class1 class2 class3">3-2</div></td>
+            <td class="column3"><div class="class1 class2 class3" style="background-color: blue; animation: move2 1s; animation-fill-mode: forwards;">3-3</div></td>
+            <td class="column4"><div class="class1 class2 class3">3-4</div></td>
+            <td class="column5"><div class="class1 class2 class3">3-5</div></td>
+            <td class="column6"><div class="class1 class2 class3">3-6</div></td>
+            <td class="column7"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-7</div></td>
+            <td class="column8"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-8</div></td>
+          </tr>
+          <tr id="row5">
+            <td class="column1"><div class="class1 class2 class3">4-1</div></td>
+            <td class="column2"><div class="class1 class2 class3">4-2</div></td>
+            <td class="column3"><div class="class1 class2 class3">4-3</div></td>
+            <td class="column4"><div class="class1 class2 class3">4-4</div></td>
+            <td class="column5"><div class="class1 class2 class3">4-5</div></td>
+            <td class="column6"><div class="class1 class2 class3">4-6</div></td>
+            <td class="column7"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-7</div></td>
+            <td class="column8"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-8</div></td>
+          </tr>
+          <tr id="row6">
+            <td class="column1"><div class="class1 class2 class3">5-1</div></td>
+            <td class="column2"><div class="class1 class2 class3">5-2</div></td>
+            <td class="column3"><div class="class1 class2 class3">5-3</div></td>
+            <td class="column4"><div class="class1 class2 class3">5-4</div></td>
+            <td class="column5"><div class="class1 class2 class3">5-5</div></td>
+            <td class="column6"><div class="class1 class2 class3">5-6</div></td>
+            <td class="column7"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-7</div></td>
+            <td class="column8"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-8</div></td>
+          </tr>
+          <tr id="row7">
+            <td class="column1"><div class="class1 class2 class3">6-1</div></td>
+            <td class="column2"><div class="class1 class2 class3">6-2</div></td>
+            <td class="column3"><div class="class1 class2 class3">6-3</div></td>
+            <td class="column4"><div class="class1 class2 class3">6-4</div></td>
+            <td class="column5"><div class="class1 class2 class3">6-5</div></td>
+            <td class="column6"><div class="class1 class2 class3">6-6</div></td>
+            <td class="column7"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-7</div></td>
+            <td class="column8"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-8</div></td>
+          </tr>
+          <tr id="row8">
+            <td class="column1"><div class="class1 class2 class3">7-1</div></td>
+            <td class="column2"><div class="class1 class2 class3">7-2</div></td>
+            <td class="column3"><div class="class1 class2 class3">7-3</div></td>
+            <td class="column4"><div class="class1 class2 class3">7-4</div></td>
+            <td class="column5"><div class="class1 class2 class3">7-5</div></td>
+            <td class="column6"><div class="class1 class2 class3">7-6</div></td>
+            <td class="column7"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-7</div></td>
+            <td class="column8"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-8</div></td>
+          </tr>
+          <tr id="row9">
+            <td class="column1"><div class="class1 class2 class3">8-1</div></td>
+            <td class="column2"><div class="class1 class2 class3">8-2</div></td>
+            <td class="column3"><div class="class1 class2 class3">8-3</div></td>
+            <td class="column4"><div class="class1 class2 class3">8-4</div></td>
+            <td class="column5"><div class="class1 class2 class3">8-5</div></td>
+            <td class="column6"><div class="class1 class2 class3">8-6</div></td>
+            <td class="column7"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-7</div></td>
+            <td class="column8"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-8</div></td>
+          </tr>
+          <tr id="row10">
+            <td class="column1"><div class="class1 class2 class3">9-1</div></td>
+            <td class="column2"><div class="class1 class2 class3">9-2</div></td>
+            <td class="column3"><div class="class1 class2 class3">9-3</div></td>
+            <td class="column4"><div class="class1 class2 class3">9-4</div></td>
+            <td class="column5"><div class="class1 class2 class3">9-5</div></td>
+            <td class="column6"><div class="class1 class2 class3">9-6</div></td>
+            <td class="column7"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-7</div></td>
+            <td class="column8"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-8</div></td>
+          </tr>
+          <tr id="row11">
+            <td class="column1"><div class="class1 class2 class3">10-1</div></td>
+            <td class="column2"><div class="class1 class2 class3">10-2</div></td>
+            <td class="column3"><div class="class1 class2 class3">10-3</div></td>
+            <td class="column4"><div class="class1 class2 class3">10-4</div></td>
+            <td class="column5"><div class="class1 class2 class3">10-5</div></td>
+            <td class="column6"><div class="class1 class2 class3">10-6</div></td>
+            <td class="column7"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-7</div></td>
+            <td class="column8"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-8</div></td>
+          </tr>
+          <tr id="row12">
+            <td class="column1"><div class="class1 class2 class3">11-1</div></td>
+            <td class="column2"><div class="class1 class2 class3">11-2</div></td>
+            <td class="column3"><div class="class1 class2 class3">11-3</div></td>
+            <td class="column4"><div class="class1 class2 class3">11-4</div></td>
+            <td class="column5"><div class="class1 class2 class3">11-5</div></td>
+            <td class="column6"><div class="class1 class2 class3">11-6</div></td>
+            <td class="column7"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-7</div></td>
+            <td class="column8"><div class="class1 class2 class3" style="background-color: darkturquoise; animation: move1 1s; animation-fill-mode: forwards;">1-8</div></td>
+          </tr>
+        </table>
+      </div>
     </center>
     <br>
-    <div class="test"></div>
     <footer>
     </footer>
   </body>
